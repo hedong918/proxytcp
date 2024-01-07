de2ade41bb8055d99454e53517ab8e71fd2c5691
hedong918 <155141322+hedong918@users.noreply.github.com>
2024-01-07 10:28:29 +0800

    Update main.rs
    添加负载方式

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::net::IpAddr;
 use std::net::SocketAddr;
 use tokio::io;
 use tokio::net::{TcpStream, TcpListener};
@@ -8,26 +10,62 @@ use chrono::Local;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
+
+    //获取命令行参数
+    let mut args: Vec<String> = env::args().collect();
+
+    //校验参数数量
+    if args.len() < 4 {
         panic!("请正确输入启动参数！");
     }
 
+    //获取本机监听地址
     let local_addr = args[1].clone();
-    let server_addr = args[2].clone();
+    
+    //获取代理类型
+    let proxy_mod = match args[2].clone().as_str() {
+        "iphash" => {Mode::Iphash} ,
+        "poll" => {Mode::Poll},
+        _ => {
+            panic!("代理类型输入错误，可用参数：iphash、poll！");
+        },
+
+    };
+
+    //获取服务器组
+    let mut server_addrs:Vec<String> =vec![];
+    
+    for i in args[3..].iter(){
+        server_addrs.push(i.to_string());
+    }
+
+    let mut num = 0;
+    let mut map = HashMap::new();
+
+
+    //打印启动信息
     let now = Local::now();
-    println!("{} 代理程序启动！监听地址:{};目标服务器地址:{}",now, local_addr, server_addr);
+    println!("{} 代理程序启动！监听地址:{};目标服务器地址:{:?}",now, local_addr, server_addrs);
 
-
+    //开始监听本地地址
     let listener = TcpListener::bind(local_addr).await.expect("无法监听该端口！");
+
+    //进入循环处理状态
     loop {
+        //获取客户端连接
         let (mut socket, addr) = listener.accept().await.expect("系统故障无法继续监听！");
-        match tcpproxy(socket, addr, server_addr.clone()).await {
-            Ok(_) => {}
+
+
+        //根据代理类型获取服务器地址
+        let server_addr = choose_server(&proxy_mod,&server_addrs,&mut num,&mut map,addr);
+
+        //进行数据转发
+        match tcpproxy(socket, addr, server_addr).await {
+            Ok(_) => {},
             Err(e) => {
                 let now = Local::now();
                 println!("{} 和服务端连接出现错误！:{}",now,e);
-            }
+            },
         }
     }
 }
@@ -50,4 +88,45 @@ async fn tcpproxy(mut socket: TcpStream, addr: SocketAddr, server_addr: String) 
     // });
 
     Ok(())
+}
+
+enum Mode {
+    Iphash,
+    Poll,
+}
+
+
+fn choose_server(mode:&Mode,addrs:&Vec<String>,num:&mut usize,map:&mut HashMap<IpAddr,usize>,addr:SocketAddr) -> String {
+    match mode {
+        Mode::Iphash => {
+            let ip = addr.ip();
+            match map.get(&ip) {
+                Some(x) => {
+                    addrs[*x].clone()
+                },
+                None => {
+                    let max = addrs.len() -1;
+                    if *num < max {
+                        *num += 1;
+                        map.insert(ip, *num);
+                        addrs[(*num-1)].clone()
+                    }else {
+                        *num = 0;
+                        map.insert(ip, max);
+                        addrs.last().unwrap().clone()
+                    }
+                },
+            }
+        },
+        Mode::Poll => {
+            let max = addrs.len() -1;
+            if *num < max {
+                *num += 1;
+                addrs[(*num-1)].clone()
+            }else {
+                *num = 0;
+                addrs.last().unwrap().clone()
+            }
+        },
+    }
 }
